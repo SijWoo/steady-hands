a706d3a6d6eef2b053d4abe8da204bd9d78016b6
Sijin Woo <sijin.woo@utexas.edu>
2020-06-25 00:16:19 -0500

    Creates prototypes for Drv_Motor

--- firmware/bsp/inc/bsp_motor.h
+++ firmware/bsp/inc/bsp_motor.h
@@ -12,9 +12,9 @@ void BSP_Motor_Init(void);
 
 /**
  * @brief   Set the duty cycle of the PWM pin.
- * @param   duty    Fixed point integer with resolution 1 and range [0,100].
+ * @param   duty_fixpt  Fixed point integer with resolution 1 and range [0,100].
  * @return  None
  */
-void BSP_Motor_SetDuty(uint8_t duty);
+void BSP_Motor_SetDuty(uint8_t duty_fixpt);
 
 #endif
--- firmware/drv/motor/inc/drv_motor.h
+++ firmware/drv/motor/inc/drv_motor.h
@@ -3,6 +3,20 @@
 
 #include "common.h"
 
+/**
+ * @brief   Initializes the Motor Driver
+ * @param   None
+ * @return  None
+ */
+void Drv_Motor_Init(void);
 
+/**
+ * @brief   Sets the intensity of the vibrating motor
+ * @param   intense_fixpt   Fixed point integer with resolution 1 and
+ *                          range [0,100]. Gauranteed no vibration at 0 and max
+ *                          vibration at 100.
+ * @return  None
+ */
+void Drv_Motor_SetIntensity(uint8_t intense_fixpt);
 
 #endif
