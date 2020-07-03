bf64c1cbc7359081d57e0a7da95ac14bf5b8fae1
Sijin Woo <sijin.woo@utexas.edu>
2020-07-02 22:02:34 -0500

    Adds new BSP src files for new structure since some bsp modules are used multiple times in the drivers

---
+++ firmware/bsp/inc/bsp_gpio.h
@@ -0,0 +1,38 @@
+#ifndef __BSP_GPIO_H
+#define __BSP_GPIO_H
+
+#include "common.h"
+
+/**
+ * @brief   Initializes the GPIO ports/pins that are connected to the modules.
+ * @note    The necessary input pins that requires an interrupt are initialized.
+ * @note    The module pins can be differentiated with the <x>id_t types.
+ *          Look in common.h for more info.
+ * @param   None
+ * @return  None
+ */
+void BSP_GPIO_Init(void);
+
+/**
+ * @brief   Attaches a handler to an interrupt pin.
+ * @param   iid     The module's INT ID
+ * @param   handler Function that will run whenever there INT pin it triggered.
+ */
+void BSP_GPIO_AttachISR(intid_t iid, void (*handler)(void));
+
+/**
+ * @brief   Sets the state of the Reset Pin of the specific module.
+ * @param   rid     The module's reset ID.
+ * @param   state   LOW or HIGH
+ * @return  None
+ */
+void BSP_GPIO_SetRstState(rstid_t rid, pin_state_t state);
+
+/**
+ * @brief   Gets the state of the INT pin of the specific module.
+ * @param   iid     The module's INT ID.
+ * @return  pin_state_t (LOW or HIGH)
+ */
+pin_state_t BSP_GPIO_GetIntState(intid_t iid);
+
+#endif
--- firmware/bsp/inc/bsp_pwm.h
+++ firmware/bsp/inc/bsp_pwm.h
@@ -1,5 +1,5 @@
-#ifndef __BSP_MOTORS_H
-#define __BSP_MOTORS_H
+#ifndef __BSP_PWM_H
+#define __BSP_PWM_H
 
 #include "common.h"
 
@@ -8,7 +8,7 @@
  * @param   None
  * @return  None
  */
-void BSP_Motors_Init(void);
+void BSP_PWM_Init(void);
 
 /**
  * @brief   Set the duty cycle of the specified PWM pin.
@@ -16,6 +16,6 @@ void BSP_Motors_Init(void);
  * @param   duty_fixpt  Fixed point integer with resolution 1 and range [0,100].
  * @return  None
  */
-void BSP_Motors_SetDuty(motorid_t mid, uint8_t duty_fixpt);
+void BSP_PWM_SetDuty(motorid_t mid, uint8_t duty_fixpt);
 
 #endif
